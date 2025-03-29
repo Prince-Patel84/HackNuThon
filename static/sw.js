e084946be8875c981dd3f7915eed17cf653a2442
Prince Patel <princegithub84@gmail.com>
2025-03-30 01:03:57 +0530

    Version 1.1 - Optimized for mobile Uses

---
+++ static/sw.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = 'industrial-docs-v1';
+const urlsToCache = [
+    '/',
+    '/static/css/bootstrap.min.css',
+    '/static/js/bootstrap.bundle.min.js',
+    '/static/css/all.min.css',
+    '/static/js/all.min.js'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Activate service worker
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch event handler
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Return cached response if found
+                if (response) {
+                    return response;
+                }
+
+                // Clone the request
+                const fetchRequest = event.request.clone();
+
+                // Make network request
+                return fetch(fetchRequest).then(response => {
+                    // Check if response is valid
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+
+                    // Clone the response
+                    const responseToCache = response.clone();
+
+                    // Cache the response
+                    caches.open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(event.request, responseToCache);
+                        });
+
+                    return response;
+                });
+            })
+    );
+}); 
